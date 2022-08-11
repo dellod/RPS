095f854095afd9d4acef7fa892af3f34ac0a6405
dellod <daryl.dang4@gmail.com>
2022-08-11 13:25:40 -0600

    Add win condition checking

--- scripts/index.js
+++ scripts/index.js
@@ -21,9 +21,39 @@ function changePlayerChoice(choice, player_image_name) {
     }
 }
 
+function determineWinnerAndUpdateResults(player1Choice, player2Choice) {
+    const results_label = document.querySelector(".results");
+
+    if(player1Choice === player2Choice) {
+        results_label.textContent = "Tie!";
+    }
+    else if(player1Choice === 0) {
+        if(player2Choice === 1) {
+            results_label.textContent = "Player 2 wins!";
+        } else if(player2Choice === 2) {
+            results_label.textContent = "Player 1 wins!";
+        }
+    }
+    else if(player1Choice === 1) {
+        if(player2Choice === 0) {
+            results_label.textContent = "Player 1 wins!";
+        } else if(player2Choice === 2) {
+            results_label.textContent = "Player 2 wins!";
+        }
+    }
+    else if(player1Choice === 2) {
+        if(player2Choice === 0) {
+            results_label.textContent = "Player 2 wins!";
+        } else if(player2Choice === 1) {
+            results_label.textContent = "Player 1 wins!";
+        }
+    }
+}
+
 function shootPlayerChoices() {
     const player1Choice = generatePlayerChoice();
     const player2Choice = generatePlayerChoice();
     changePlayerChoice(player1Choice, ".player1-image");
     changePlayerChoice(player2Choice, ".player2-image");
+    determineWinnerAndUpdateResults(player1Choice, player2Choice)
 }
